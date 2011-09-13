5c2b52999b57ff6cedeaef8db6b960244c19e1a2
Amber Feng <amber.feng@gmail.com>
2011-09-13 00:04:30 -0700

    got mult senders to work possibly (haven't checked yet)

--- hw1/fifo.rb
+++ hw1/fifo.rb
@@ -15,12 +15,13 @@ module FIFODelivery
     channel :pipe_channel, [:@dst, :src, :ident] => [:payload]
     scratch :pipe_chan, [:dst, :src, :ident] => [:payload]
     table :intermediate, [:dst, :src, :ident] => [:payload]
-    table :current_ident, [] => [:ident]
+    table :current_idents, [:src] => [:ident]
+    scratch :current_ident, [] => [:src, :ident]
     scratch :current, [] => [:dst, :src, :ident, :payload]
   end
 
-  bootstrap do
-    current_ident <= [[0]]
+  bootstrap do 
+    current_idents <= [["localhost:54321", 0]]
   end
 
   bloom :snd do
@@ -34,20 +35,40 @@ module FIFODelivery
     intermediate <= pipe_channel
   end
 
+  bloom :counter_init do
+    # If the counter for something that just came in does not exist yet, create it
+
+    #current_idents <+ pipe_channel {|x| [x.src, 0] unless (((current_idents * pipe_channel).pairs (:src => :src)).exists?) }
+    temp :check_exists <= (pipe_channel * current_idents).pairs {|u, c| [c.src, c.ident] if u.src == c.src}
+    #stdio <~ check_exists
+    current_idents <+ pipe_channel {|x| [x.src, 0] unless check_exists.length > 0 } 
+    #stdio <~ current_ident
+
+    #current_idents <= (current { |x| [x.src, 0] } if (((current * current_idents).pairs {|u, c| [] if u.src == c.src }).length == 0))
+
+    #current_ident <= (((current * current_idents).pairs {|u, c| [c.src, c.ident] if u.src == c.src }) or ([ current { |x| [x.src, 0] } ])) 
+    current_idents <+- current_ident {|c| [c.src, c.ident + 1] }
+
+    stdio <~ [current_idents {|c| ["#{c.src}, #{c.ident}"]}, ["---"]]
+    #stdio <~ current_ident {|c| ["#{c.src}, #{c.ident}"]}
+  end
+
   bloom :process do
     # On the receiver, add the packets in order to pipe_chan 
     #stdio <~ [["tick #{current_ident {|i| i.ident}} at #{budtime}"]]
 
-    # Find the next packet to add to pipe_chan (according to the counter)
-    current <= (intermediate * current_ident).pairs { |p, i| p if p.ident == i.ident} 
+    # Find the next packet to add to pipe_chan (according to the counter for this specific sender)
+    current <= (intermediate * current_idents).pairs { |p, i| p if p.ident == i.ident and p.src == i.src} 
     pipe_chan <+ current
     
     # Remove from intermediate
     intermediate <- current
 
-    # Update counter
-    current_ident <- (current * current_ident).pairs {|u, c| [c.ident] }
-    current_ident <+ (current * current_ident).pairs {|u, c| [c.ident + 1]}
+    # Increment counter for this specific sender
+    current_ident <= (current * current_idents).pairs {|u, c| [c.src, c.ident] if u.src == c.src }
+
+    current_idents <+- current_ident {|c| [c.src, c.ident + 1] }
+
   end
 
   bloom :done do
