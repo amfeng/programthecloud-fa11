246c06792d3ca7c71dd931e8907aa8e09b7805ca
Amber Feng <amber.feng@gmail.com>
2011-09-15 11:31:16 -0700

    skeleton code/structure for hw2

---
+++ hw2/lckmgr.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'bud'
+
+module LockMgrProtocol
+  state do
+    interface input, :request_lock, [:xid, :resource] => [:mode]
+    interface input, :end_xact, [:xid]
+    interface output, :lock_status, [:xid, :resource] => [:status]
+  end
+end
+
+module LockMgr
+  include LockMgrProtocol
+end
