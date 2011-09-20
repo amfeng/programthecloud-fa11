93d5f674e433fb6757bbe74189fb1a553abdfc1d
Amber Feng <amber.feng@gmail.com>
2011-09-19 21:54:27 -0700

    merge

--- hw2/lckmgr.rb
+++ hw2/lckmgr.rb
@@ -21,57 +21,78 @@ module TwoPhaseLockMgr
 
     table :locks, [:xid, :resource] => [:mode]
 
-    # FIXME: Better to keep redundant data or regenerate on the fly?
+    # TODO: Better to keep redundant data or regenerate on the fly?
     scratch :write_locks, [:resource]
   end
 
   # Some locks have restrictions on the number of locks on a resource,
   # so doesn't make sense to process more than the allowed amount
   bloom :gatekeeper do
+    stdio <~ [["tick #{budtime}"]]
     # Allowing reads (:S lock)
 
     # Add shared lock requests coming in to the read queue
     read_queue <+ request_lock.select { |r| r.mode == "S" } 
+    #stdio <~ read_queue.inspected
 
     # No restrictions on how many shared locks allowed on a resource
     # at once (unless there's an exclusive lock), we'll let them all
     # in for processing
     request_read <+ read_queue
     read_queue <- read_queue
+    #stdio <~ request_read.inspected
 
     # Allowing writes (:X lock)
     
     # Add exclusive lock requests coming in to the read queue
     write_queue <+ request_lock.select { |r| r.mode == "X" } 
+    #stdio <~ write_queue.inspected
    
     # At most 1 exclusive lock per resource at a time, so we'll choose one per 
     # resource to process
-    temp :allow_writereq <= write_queue.group([:resource], choose(:xid))
+    temp :allow_writereq <= write_queue.group([:resource, :mode], choose(:xid))
+    #stdio <~ allow_writereq.inspected
 
-    request_write <+ allow_writereq
-    write_queue <- allow_writereq
-
-    write_queue <+ request_lock.notin(allow_writereq)
+    # Reorder columns because they got messed up in the grouping
+    request_write <+ allow_writereq {|r| [r[2], r[0], r[1]] }
+    write_queue <- allow_writereq {|r| [r[2], r[0], r[1]] }
   end
 
   # For each read request that comes in, check if we can grant the lock:
   # if so, grant it, else, add it to the appropriate queue
   bloom :process_read do
-    # Can grant read lock if currently no write locks on the resource
+    # Can grant read lock if currently no write locks on the resource held by 
+    # any other transaction
     write_locks <= locks { |l| l if l.mode == :X }
-    temp :can_read <= request_read.notin(write_locks, :resource => :resource)
+
+    # Don't downgrade lock; if already have X lock on a resource, ignore any S
+    # lock requests
+    temp :can_read <= request_read.notin(write_locks, :resource => :resource) 
+    #stdio <~ write_locks.inspected
+    #stdio <~ request_read.inspected
+    #stdio <~ locks.inspected
+    #stdio <~ can_read.inspected
 
     locks <+ can_read
+
+    # TODO: If already have X lock, remove redundant S lock request from read_queue 
     read_queue <+ request_read.notin(can_read)
    end
 
   # For each write request that comes in, check if we can grant the lock:
   # if so, grant it, else, add it to the appropriate queue
   bloom :process_write do
-    # Can grant write lock if currently no other locks on the resource
-    temp :can_write <= request_write.notin(locks, :resource => :resource)
+    # Can grant write lock if currently no other locks on the resource held
+    # by any other transaction
+    temp :can_write <= request_write.notin(locks, :resource => :resource) { |r, l| true if r.xid != l.xid }
+    #stdio <~ can_write.inspected
+ 
+    locks <+- can_write
 
-    locks <+ can_write
+    # Remove any shared locks if upgrading from S to X
+    # locks <- can_write  
+    stdio <~ locks.inspected
+
     write_queue <+ request_write.notin(can_write)
   end
 
@@ -79,5 +100,8 @@ module TwoPhaseLockMgr
   # transaction held
   bloom :remove_locks do
     locks <- (locks * end_xact).lefts(:xid => :xid)
+
+    # Remove pending locks as well, in case a transaction ended abruptly
+    # before getting all of the locks
   end
 end
--- hw2/tests/lckmgr_tests.rb
+++ hw2/tests/lckmgr_tests.rb
@@ -8,11 +8,11 @@ class Locker
   include TwoPhaseLockMgr 
 
   state do
-    table :acquired_locks , [:time, :xid, :resource, :mode]
+    table :acquired_locks, [:xid, :resource, :mode]
   end
 
   bloom do
-    acquired_locks <= locks {|r| [budtime, r.xid, r.resource, r.mode]}
+    acquired_locks <= locks {|r| [r.xid, r.resource, r.mode]}
   end
 
 end
@@ -25,7 +25,7 @@ class TestLockMgr < Test::Unit::TestCase
 
     # Acquire a shared lock
     lm.sync_do { lm.request_lock <+ [ ["1", "A", "S"] ] }
-    2.times {lm.sync_do}
+    3.times {lm.sync_do}
 
     lm.acquired_locks.each do |l|
       if l.resource == "A"
@@ -33,10 +33,11 @@ class TestLockMgr < Test::Unit::TestCase
         assert_equal(l.mode, "S")
       end
     end
+    assert_equal(lm.acquired_locks.length, 1)
 
     # Acquire an exclusive lock
     lm.sync_do { lm.request_lock <+ [ ["2", "B", "X"] ] }
-    2.times {lm.sync_do}
+    3.times {lm.sync_do}
 
     lm.acquired_locks.each do |l|
       if l.resource == "B"
@@ -44,36 +45,40 @@ class TestLockMgr < Test::Unit::TestCase
         assert_equal(l.mode, "X")        
       end
     end
+    assert_equal(lm.acquired_locks.length, 2)
 
     # Trying to acquire a :X lock on a resource when 
     # another Xact has a :S lock on it
-    # lm.sync_do { lm.request_lock <+ [ ["3", "A", "X"] ] }
-    # 2.times {lm.sync_do}
+    lm.sync_do { lm.request_lock <+ [ ["3", "A", "X"] ] }
+    3.times {lm.sync_do}
 
-    # lm.acquired_locks.each do |l|
-    #   if l.resource == "A"
-    #     assert_equal(l.xid, "1")
-    #     assert_equal(l.mode, "S")        
-    #   end
-    # end
+    lm.acquired_locks.each do |l|
+      if l.resource == "A"
+        assert_equal(l.xid, "1")
+        assert_equal(l.mode, "S")        
+      end
+    end
+    puts lm.acquired_locks.inspected
+    assert_equal(lm.acquired_locks.length, 2)
 
     # Trying to acquire a :S lock on a resource that 
     # another Xact has a :X lock on
-    # lm.sync_do { lm.request_lock <+ [ ["3", "B", "S"] ] }
-    # 2.times {lm.sync_do}
+    lm.sync_do { lm.request_lock <+ [ ["3", "B", "S"] ] }
+    3.times {lm.sync_do}
 
-    # lm.acquired_locks.each do |l|
-    #   if l.resource == "B"
-    #     assert_equal(l.xid, "2")
-    #     assert_equal(l.mode, "X")        
-    #   end
-    # end
+    lm.acquired_locks.each do |l|
+      if l.resource == "B"
+        assert_equal(l.xid, "2")
+        assert_equal(l.mode, "X")        
+      end
+    end
+    puts lm.acquired_locks.inspected
+    assert_equal(lm.acquired_locks.length, 2)
 
     # TODO: Clarify if a Xact has an exclusive lock, can it acquire
     # a shared lock. Will that result in a downgrade?
     
-    # Trying to acquire a :S lock on a resource when 
-    # the same Xact already has a :X lock on it
+    # Trying to acquire a :S lock on a resource it has a :X lock on
     # lm.sync_do { lm.request_lock <+ [ ["2", "B", "S"] ] }
     # 2.times {lm.sync_do}
 
