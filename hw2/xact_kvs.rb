b178b30264a67b0769a795a015481c7bd7c1f279
Anirudh Todi <anirudhtodi2003@gmail.com>
2011-09-20 14:56:25 -0700

    Implementing puts - half working

--- hw2/xact_kvs.rb
+++ hw2/xact_kvs.rb
@@ -4,6 +4,7 @@ require 'kvs/kvs'
 require '../lckmgr'
 
 module XactKVSProtocol
+  include BasicKVS
   state do
     interface input, :xput, [:xid, :key, :reqid] => [:data]
     interface input, :xget, [:xid, :key, :reqid]
@@ -15,20 +16,42 @@ end
 module TwoPLTransactionalKVS
   include XactKVSProtocol
   include TwoPhaseLockMgr
-  # FIXME: Not sure if I should be importing or including. Thoughts?
-  import BasicKVS => :bkvs
+
+  state do
+    table :put_queue, [:xid, :key, :reqid] => [:data]
+  end
 
   # Perform the puts
   bloom :mutate do
-    request_lock <+ xput {|x| [x.xid, x.key, :X]}
-    bkvs.kvput <+ (xput * lock_status).lefts(:xid => :xid, :key => :resource) 
-    # Not sure how to populate xput_response
+    # Request a :X lock before performing the put
+    request_lock <= xput {|x| [x.xid, x.key, :X]}
+
+    # Remember that we wanted to do a put
+    put_queue <= xput
+    
+    # Once we have obtained the lock, send that put request to basickvs
+    kvput <= (put_queue * lock_status).lefts(:xid => :xid, :key => :resource) 
+
+    # Update xput_response to indicate that we are done
+    xput_response <= (put_queue * lock_status).lefts(:xid => :xid, :key => :resource) {|put| [put.xid, put.key, put.reqid]}
+    
+    # Remove the put request from put_queue
+    put_queue <- (put_queue * xput_response).lefts(:xid => :xid, :key => :key, :reqid => :reqid)
   end
 
   # Perform the gets
   bloom :get do
     request_lock <+ xget {|x| [x.xid, x.key, :S]}
-    bkvs.kvget <+ (xget * lock_status).lefts(:xid => :xid, :key => :resource)
-    xget_response <+ bkvs.kvget_response {|x| [x.xid, x.key, x.xid, x.value]}
+    kvget <+ (xget * lock_status).lefts(:xid => :xid, :key => :resource)
+    xget_response <+ (kvget_response * xget).pairs(:key => :key) {|resp, get| [get.xid, resp.key, resp.reqid, resp.value]}
+  end
+
+  bloom :debug do
+    stdio <~ [["tick #{budtime}"]]
+    stdio <~ xput.inspected
+    stdio <~ xget.inspected
+    stdio <~ request_lock.inspected
+    stdio <~ locks.inspected
+    stdio <~ lock_status
   end
 end
