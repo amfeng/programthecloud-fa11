3ea4d673d2eef79936de3a4dc39ac043cdc3323d
Anirudh Todi <anirudhtodi@berkeley.edu>
2011-10-26 22:36:17 -0700

    Fixed some path dependencies

--- hw5/tests/test_2pc.rb
+++ hw5/tests/test_2pc.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'bud'
 require 'test/unit'
-require '../quorum_kvs'
+require 'twophasecommit'
 
-class TestQuorum < Test::Unit::TestCase
+class Test2PC < Test::Unit::TestCase
   class TwoPCTest
     include Bud
     include TwoPCParticipant
@@ -21,7 +21,7 @@ class TestQuorum < Test::Unit::TestCase
                                        [4, 4, 'localhost:54323']]}
     
     # Broadcast a commit request - it should succeed
-    resps = p1.sync_callback (:commit_request, [[5]], :commit_response)
+    resps = p1.sync_callback(:commit_request, [[5]], :commit_response)
     assert_equal([["C"]], resps)
     
     # # Pause participant 1
@@ -42,14 +42,14 @@ class TestQuorum < Test::Unit::TestCase
     p1.sync_callback {p1.pause_participant <+ [[10,2]]}
     
     # Broadcast a commit request - it should fail
-    resps = p1.sync_callback (:commit_request, [[11]], :commit_response)
+    resps = p1.sync_callback(:commit_request, [[11]], :commit_response)
     assert_equal([["A"]], resps)
 
     # Resume participant 2
     p1.sync_callback {p1.pause_participant <+ [[12,2]]}
     
     # Broadcast a commit request - it should succeed
-    resps = p1.sync_callback (:commit_request, [[13]], :commit_response)
+    resps = p1.sync_callback(:commit_request, [[13]], :commit_response)
     assert_equal([["C"]], resps)
 
     p1.stop
