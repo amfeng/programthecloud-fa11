e8e3e0dbb2091db2abb82ef531634ed30355c7b2
Eric Shih <ericbshih@gmail.com>
2011-11-13 01:10:39 -0800

    updated tc_voting2.rb (only) with more tests and made it more DRY

--- voting/tc_voting2.rb
+++ voting/tc_voting2.rb
@@ -14,6 +14,17 @@ class TestVoting < Test::Unit::TestCase
     include UnanimousVoteCounter
   end
 
+  class MajorityVotingBloom
+    include Bud
+    include MajorityVoteCounter
+  end
+
+  def basic_checks(expected_id, expected_status, expected_response, resps)
+    assert_equal(expected_id, resps.first[0])
+    assert_equal(expected_status, resps.first[1])
+    assert_equal(expected_response, resps.first[2])
+  end
+
   def test_ratio_voter_success
     p1 = RatioVotingBloom.new
     p1.run_bg
@@ -25,9 +36,8 @@ class TestVoting < Test::Unit::TestCase
     #p1.sync_do {p1.cast_vote <+ [[1, 'Obama', 'second']]}
     resps = p1.sync_callback(p1.cast_vote.tabname, [[1, 'Obama', 'second']], 
                              p1.result.tabname)
-    assert_equal(1, resps.first[0])
-    assert_equal(:success, resps.first[1])
-    assert_equal('Obama', resps.first[2])
+    basic_checks(1, :success, 'Obama', resps)
+    # Additional assertions
     assert_equal(['Obama', 'Obama'], resps.first[3])
     assert_equal(true, resps.first[4].include?('second'))
     assert_equal(true, resps.first[4].include?('first'))
@@ -39,8 +49,7 @@ class TestVoting < Test::Unit::TestCase
     p1.sync_do {p1.cast_vote <+ [[2, 'McCain', 'second']]}
     resps = p1.sync_callback(p1.cast_vote.tabname, [[2, 'Obama', 'third']], 
                              p1.result.tabname)
-    assert_equal(2, resps.first[0])
-    assert_equal(:success, resps.first[1])
+    basic_checks(2, :success, 'Obama', resps)
     
     p1.stop
   end
@@ -55,8 +64,7 @@ class TestVoting < Test::Unit::TestCase
     p1.sync_do {p1.cast_vote <+ [[1, 'Obama', 'first']]}
     resps = p1.sync_callback(p1.cast_vote.tabname, [[1, 'McCain', 'second']], 
                              p1.result.tabname)
-    assert_equal(1, resps.first[0])
-    assert_equal(:fail, resps.first[1])
+    basic_checks(1, :fail, nil, resps)
 
     # Test failure given a ratio of 0.5
     p1.sync_do {p1.begin_vote <+ [[2, 3]]}
@@ -65,8 +73,7 @@ class TestVoting < Test::Unit::TestCase
     p1.sync_do {p1.cast_vote <+ [[2, 'McCain', 'second']]}
     resps = p1.sync_callback(p1.cast_vote.tabname, [[2, 'Nader', 'third']], 
                              p1.result.tabname)
-    assert_equal(2, resps.first[0])
-    assert_equal(:fail, resps.first[1])
+    basic_checks(2, :fail, nil, resps)
 
     p1.stop
   end
@@ -80,9 +87,8 @@ class TestVoting < Test::Unit::TestCase
     p1.sync_do {p1.cast_vote <+ [[1, 'Obama', 'first']]}
     resps = p1.sync_callback(p1.cast_vote.tabname, [[1, 'Obama', 'second']], 
                              p1.result.tabname)
-    assert_equal(1, resps.first[0])
-    assert_equal(:success, resps.first[1])
-    assert_equal('Obama', resps.first[2])
+    basic_checks(1, :success, 'Obama', resps)
+    # Additional checks
     assert_equal(['Obama', 'Obama'], resps.first[3])
     assert_equal(true, resps.first[4].include?('second'))
     assert_equal(true, resps.first[4].include?('first'))
@@ -92,11 +98,46 @@ class TestVoting < Test::Unit::TestCase
     p1.sync_do {p1.cast_vote <+ [[2, 'Obama', 'first']]}
     resps = p1.sync_callback(p1.cast_vote.tabname, [[2, 'McCain', 'second']], 
                              p1.result.tabname)
-    
-    assert_equal(2, resps.first[0])
-    assert_equal(:fail, resps.first[1])
+    basic_checks(2, :fail, nil, resps)
     
     p1.stop
   end
-  
+
+  def test_majority_voter
+    p1 = MajorityVotingBloom.new
+    p1.run_bg
+
+    # Test success in a 2 agent case (need both)
+    p1.sync_do {p1.begin_vote <+ [[1, 2]]}
+    p1.sync_do {p1.cast_vote <+ [[1, 'Obama', 'first']]}
+    resps = p1.sync_callback(p1.cast_vote.tabname, [[1, 'Obama', 'second']], 
+                             p1.result.tabname)
+    basic_checks(1, :success, 'Obama', resps)
+    # Additional checks
+    assert_equal(['Obama', 'Obama'], resps.first[3])
+    assert_equal(true, resps.first[4].include?('second'))
+    assert_equal(true, resps.first[4].include?('first'))
+
+    # Test success in a 4 agent case (need 3)
+    p1.sync_do {p1.begin_vote <+ [[2, 4]]}
+    p1.sync_do {p1.cast_vote <+ [[2, 'Obama', 'first']]}
+    p1.sync_do {p1.cast_vote <+ [[2, 'Obama', 'second']]}
+    p1.sync_do {p1.cast_vote <+ [[2, 'McCain', 'third']]}
+    resps = p1.sync_callback(p1.cast_vote.tabname, [[2, 'Obama', 'fourth']], 
+                             p1.result.tabname)
+    basic_checks(2, :success, 'Obama', resps)
+    # Additional checks
+    assert_equal(3, resps.first[3].count('Obama'))
+    assert_equal(1, resps.first[3].count('McCain'))
+
+    # Test fail in a 2 agent case (didn't get 2)
+    p1.sync_do {p1.begin_vote <+ [[3, 2]]}
+    p1.sync_do {p1.cast_vote <+ [[3, 'Obama', 'first']]}
+    resps = p1.sync_callback(p1.cast_vote.tabname, [[3, 'McCain', 'second']], 
+                             p1.result.tabname)
+    basic_checks(3, :fail, nil, resps)
+    
+    p1.stop
+  end
+
 end
