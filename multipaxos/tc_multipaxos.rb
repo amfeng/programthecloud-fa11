911e9c53208d29d77725b8d222d50c32e77f99ce
Anirudh Todi <anirudhtodi@berkeley.edu>
2011-12-11 15:53:15 -0800

    added a multi-paxos test

--- multipaxos/tc_multipaxos.rb
+++ multipaxos/tc_multipaxos.rb
@@ -23,16 +23,52 @@ class TestVoting < Test::Unit::TestCase
     end
   end
 
-  ###
-  ### Test for basic paxos version
-  ###
-  def test_paxos
+  # Test for basic paxos version
+  def test_basic_paxos
     p1 = MultiPaxosTest.new
     p1.run_bg
 
     p1.sync_callback(:request, [[1, 'a']], :result)
     assert_equal([[1, :accept]], result)
+  end
 
+  # Test for multi paxos version
+  def test_multi_paxos
+    p1 = MultiPaxosTest.new
+    p1.run_bg
+
+    p1.sync_do {p1.request <+ [[1, 'a']]}
+    p1.sync_do {p1.request <+ [[2, 'a']]}
+    p1.sync_do {p1.request <+ [[3, 'a']]}
+    p1.register_callback(:accept) do |r|
+      r.each do |row|        
+        assert_equal([[3, :accept]], result)    
+      end
+    end
+  end
+
+  # Test for multi paxos version
+  def test_multi_paxos2
+    p1 = MultiPaxosTest.new(:port=>54320)
+    p1.run_bg
+    p2 = MultiPaxosTest.new(:port=>54321)
+    p2.run_bg
+    p3 = MultiPaxosTest.new(:port=>54322)
+    p3.run_bg
+    p4 = MultiPaxosTest.new(:port=>54323)
+    p4.run_bg
+    p5 = MultiPaxosTest.new(:port=>54324)
+    p5.run_bg
+
+    p1.sync_do {p1.request <+ [[1, 'a']]}
+    p1.sync_do {p1.request <+ [[2, 'a']]}
+    p1.sync_do {p1.request <+ [[3, 'a']]}
+    p1.register_callback(:accept) do |r|
+      r.each do |row|        
+        assert_equal([[3, :accept]], result)    
+      end
+    end
+  end
     # p1.sync_do {p1.request <+ [['a', 1]]}
     # p1.register_callback(:result) do |r|
     #   r.each do |row|
@@ -41,27 +77,4 @@ class TestVoting < Test::Unit::TestCase
     #     puts row.status
     #   end
     # end
-
-    #p1.sync_do {p1.begin_vote <+ [[1, 2]]; p1.ratio <+ [[1, 1]]}
-    #p1.sync_do {p1.cast_vote <+ [[1, :A, 'Obama', 'first']]}
-
-    
-
-    #resps = p1.sync_callback(p1.cast_vote.tabname, [[1, :B, 'Obama', 'second']], p1.result.tabname)
-    #basic_checks(1, :success, 'Obama', resps)
-
-    # Check accumulated votes/notes
-    #assert_equal(['Obama', 'Obama'], resps.first[3])
-    #assert_equal(true, resps.first[4].include?('second'))
-    #assert_equal(true, resps.first[4].include?('first'))
-
-    # Test success given a ratio of 0.5
-    #p1.sync_do {p1.begin_vote <+ [[2, 3]]; p1.ratio <+ [[2, 0.5]]}
-    #p1.sync_do {p1.cast_vote <+ [[2, :A, 'Obama']]}
-    #p1.sync_do {p1.cast_vote <+ [[2, :B, 'McCain']]}
-    #resps = p1.sync_callback(p1.cast_vote.tabname, [[2, :C, 'Obama']], 
-    #                         p1.result.tabname)
-    #basic_checks(2, :success, 'Obama', resps)    
-    #p1.stop
-  end
 end
