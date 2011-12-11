e38a969d4fc805c4c45abe249a4be20f8c2ed78d
Anirudh Todi <anirudhtodi@berkeley.edu>
2011-12-10 19:08:53 -0800

    added a basic test

--- multipaxos/tc_multipaxos.rb
+++ multipaxos/tc_multipaxos.rb
@@ -11,25 +11,36 @@ class TestVoting < Test::Unit::TestCase
     #include PaxosProtocol
     #include PaxosInternalProtocol
     include Paxos
-    include BestEffortDelivery
-    include StaticMembership
+    # include BestEffortDelivery
+    # include StaticMembership
+    
+    bootstrap do
+      add_member <= [['localhost:54321', 1],
+                     ['localhost:54322', 2],
+                     ['localhost:54323', 3],
+                     ['localhost:54324', 4],
+                     ['localhost:54325', 5]]
+    end
   end
 
   ###
-  ### Test for basic poxos version
+  ### Test for basic paxos version
   ###
   def test_paxos
     p1 = MultiPaxosTest.new
     p1.run_bg
 
-    p1.sync_do {p1.request <+ [['a', 1]]}
-    p1.register_callback(:result) do |r|
-      r.each do |row|
-        puts "ROW ASSERTION"
-        assert_equal(row.ident, 'a')
-        puts row.status
-      end
-    end
+    p1.sync_callback(:request, [[1, 'a']], :result)
+    assert_equal([[1, :accept]], result)
+
+    # p1.sync_do {p1.request <+ [['a', 1]]}
+    # p1.register_callback(:result) do |r|
+    #   r.each do |row|
+    #     puts "ROW ASSERTION"
+    #     assert_equal(row.ident, 'a')
+    #     puts row.status
+    #   end
+    # end
 
     #p1.sync_do {p1.begin_vote <+ [[1, 2]]; p1.ratio <+ [[1, 1]]}
     #p1.sync_do {p1.cast_vote <+ [[1, :A, 'Obama', 'first']]}
